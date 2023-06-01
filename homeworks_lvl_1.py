e1671bf7ad8c134ccbde68c8dda09c033f00e56e
arturrave <arturrave47@gmail.com>
2023-06-02 00:36:21 +0400

    added test file

---
+++ homeworks_lvl_1.py
@@ -0,0 +1,29 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+#my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+
+# Если честно, то сидеть считать порядковый номер каждого символа вручную не особо хочется, поэтому немного забегу вперед
+# Узнаем порядковый номер каждого символа в строке, для этого воспользуемся циклом for и функцией enumerate() :
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+#for i in enumerate(my_favorite_songs):
+#    print(i) 
+
+
+
+print ("Первый трек: ", my_favorite_songs[:14],
+        "Последний трек: ", my_favorite_songs[64:],
+          "Второй трек: ", my_favorite_songs[16:30],
+          # Узнаем порядковый номер символов с конца строки:
+          #for i in enumerate(my_favorite_songs[::-1]):
+          #print(i)
+          # Поехали:
+           "Второй трек с конца: ", my_favorite_songs [-61:-47])
+
+
