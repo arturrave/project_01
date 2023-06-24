996371b0ac5d4579bc8aaf7abd20adde7c62eeaf
arturrave <134163910+arturrave@users.noreply.github.com>
2023-06-24 20:40:01 +0400

    homeworks_lvl_2_task_2.3.py download

---
+++ homeworks_lvl_2_task_2.3.py
@@ -0,0 +1,32 @@
+# Задача 2.3.
+
+# Напишите функцию, которая принимает цифры от 0 до 9 и возвращает значение прописью.
+# Например,
+# switch_it_up(1) -> 'One'
+# switch_it_up(3) -> 'Three'
+# switch_it_up(10000) -> None
+# Использовать условный оператор if-elif-else нельзя!
+
+
+def switch_it_up(number):
+    numbers_list = [
+    'Zero',
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five',
+    'Six',
+    'Seven',
+    'Eight',
+    'Nine'
+    ]
+    
+    try:
+        numeral = numbers_list[number]
+        return numeral
+    except:
+        return
+
+print(switch_it_up(1000))
+    
