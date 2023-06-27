b7ab70d3ba57b495b08534869150a81398e76c45
arturrave <134163910+arturrave@users.noreply.github.com>
2023-06-27 22:27:49 +0400

    homeworks_lvl_3_task_3.1 downloaded

---
+++ homeworks_lvl_3_task_3.1.py
@@ -0,0 +1,54 @@
+# Задача 3.1.
+# Создайте класс матрицы (или таблицы).
+# Требования к классу:
+#   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
+#   - в каждой ячейке содержится либо число, либо None
+#   - доступы следующие методы матрицы:
+#       * принимать новые значения, 
+#       * заменять существующие значения, 
+#       * выводить число строк и колонок.
+
+# Пример матрицы 10 на 10 из единиц:
+# [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# Примечание! 
+#   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
+#   - отображать в таблице/матрице название колонки не обязательно!
+#   - использовать готовые классы numpy.array() и pandas.DataFrame() запрещено!
+#   - проявите фантазию :)
+
+import pprint
+
+class Matrix:
+    def __init__(self, rows, columns):
+        self._matrix = [[None for j in range(columns)] for i in range(rows)]
+        self._rows_count = rows
+        self._columns_count = columns
+
+    def set_value(self, row, column, value):
+        self._matrix[row][column] = value
+
+    def get_value(self, row, column):
+        return self._matrix[row][column] 
+
+    def get_rows_count(self):
+        return self._rows_count
+
+    def get_columns_count(self):
+        return self._columns_count
+
+m = Matrix(10,10)
+for i in range(m.get_rows_count()):
+    for j in range(m.get_columns_count()):
+        m.set_value(i, j, 1)
+        
+pprint.pprint (m._matrix)                   
